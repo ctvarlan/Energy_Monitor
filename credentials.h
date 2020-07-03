e27c843f107cdd8aa296238ec4d94cea7f4e9da6
CTVarlan <ctvarlan@users.noreply.github.com>
2020-07-02 22:15:39 -0400

    Create credentials.h

---
+++ credentials.h
@@ -0,0 +1,9 @@
+const       char*   ssid                = "yourSSID";
+const       char*   password            = "yourPASSWORD";
+
+const       char*   ThingSpeak          = "api.thingspeak.com";
+const       String  ThingSpeak_key      = "xxxxxxxxxxxxxxxx"; //
+unsigned    long    ThingSpeak_channel  = 972755;
+
+const 	    char*   ghost               = "www.google.com";
+const	    int		httpPort            = 80;
