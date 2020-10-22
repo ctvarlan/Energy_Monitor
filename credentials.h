efe5cca1c1d0c3eeedc0082ab56f71b3059900cc
CTVarlan <ctvarlan@users.noreply.github.com>
2020-10-22 13:06:07 -0400

    Update credentials.h

--- credentials.h
+++ credentials.h
@@ -3,7 +3,7 @@ const       char*   password            = "yourPASSWORD";
 
 const       char*   ThingSpeak          = "api.thingspeak.com";
 const       String  ThingSpeak_key      = "xxxxxxxxxxxxxxxx"; //
-unsigned    long    ThingSpeak_channel  = 972755;
+unsigned    long    ThingSpeak_channel  = 000000;             //972755;
 
 const 	    char*   ghost               = "www.google.com";
 const	    int		httpPort            = 80;
